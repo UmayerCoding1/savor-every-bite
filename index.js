67b3fa38e14abf010ba34b817ae17ad425802994
unknown <umayerpro175@gmail.com>
2024-09-30 10:05:33 +0600

    update

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Adjust methods as needed
   credentials: true,
+  optionsSuccessStatus: 200
 }));
 app.use(express.json());
 
